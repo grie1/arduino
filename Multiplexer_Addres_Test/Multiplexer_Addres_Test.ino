691801ff6e16b749feecd98c066953e50e232cda
Eric G <eric.grismer@gmail.com>
2021-01-22 11:14:05 -0800

    updates

--- Multiplexer_Addres_Test/Multiplexer_Addres_Test.ino
+++ Multiplexer_Addres_Test/Multiplexer_Addres_Test.ino
@@ -1,53 +1,53 @@
-/**
- * TCA9548 I2CScanner.pde -- I2C bus scanner for Arduino
- *
- * Based on code c. 2009, Tod E. Kurt, http://todbot.com/blog/
- *
- */
+#include <Wire.h>
+const uint8_t  SCL_PIN              = 33;
+const uint8_t  SDA_PIN              = 32;
 
-#include "Wire.h"
-extern "C" { 
-#include "utility/twi.h"  // from Wire library, so we can do bus scanning
-}
-
-#define TCAADDR 0x70
-
-void tcaselect(uint8_t i) {
-  if (i > 7) return;
- 
-  Wire.beginTransmission(TCAADDR);
-  Wire.write(1 << i);
-  Wire.endTransmission();  
-}
-
-
-// standard Arduino setup()
 void setup()
 {
-    while (!Serial);
-    delay(1000);
-
-    Wire.begin();
-    
-    Serial.begin(115200);
-    Serial.println("\nTCAScanner ready!");
-    
-    for (uint8_t t=0; t<8; t++) {
-      tcaselect(t);
-      Serial.print("TCA Port #"); Serial.println(t);
-
-      for (uint8_t addr = 0; addr<=127; addr++) {
-        if (addr == TCAADDR) continue;
-      
-        uint8_t data;
-        if (! twi_writeTo(addr, &data, 0, 1, 1)) {
-           Serial.print("Found I2C 0x");  Serial.println(addr,HEX);
-        }
-      }
-    }
-    Serial.println("\ndone");
+  Wire.begin(SDA_PIN, SCL_PIN);
+  Serial.begin(115200);
+  Serial.println("\nI2C Scanner");
 }
 
-void loop() 
+
+void loop()
 {
+  byte error, address;
+  int nDevices;
+
+  Serial.println("Scanning...");
+
+  nDevices = 0;
+  for(address = 1; address < 127; address++ ) 
+  {
+    // The i2c_scanner uses the return value of
+    // the Write.endTransmisstion to see if
+    // a device did acknowledge to the address.
+    Wire.beginTransmission(address);
+    error = Wire.endTransmission();
+
+    if (error == 0)
+    {
+      Serial.print("I2C device found at address 0x");
+      if (address<16) 
+        Serial.print("0");
+      Serial.print(address,HEX);
+      Serial.println("  !");
+
+      nDevices++;
+    }
+    else if (error==4) 
+    {
+      Serial.print("Unknow error at address 0x");
+      if (address<16) 
+        Serial.print("0");
+      Serial.println(address,HEX);
+    }    
+  }
+  if (nDevices == 0)
+    Serial.println("No I2C devices found\n");
+  else
+    Serial.println("done\n");
+
+  delay(5000);           // wait 5 seconds for next scan
 }
